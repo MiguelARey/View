338414e86c22677067603e2e7314132a3f8af0c9
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-06-19 21:05:21 -0700

    Uploaded footer html and css

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="home-page">
+        <div class="newsletter">
+        <section>
+            <h1>JOIN THE VIEW</h1>
+            <p>Sign up to our newsletter so you never miss out on the product you are looking for!</p>
+        </section>
+        <form method="POST">
+            <input type="email" placeholder="Your email address" name="email">
+            <input type="submit" value="Sign Up" name="signNews">
+        </form>
+    </div>
+    <div class="container">
+
+    </div>
+    <footer>
+        <div>
+        <section>
+            <div class="links">
+                <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/892/892930.png" alt="logo-icon"></a>
+                <section class="href">
+                    <ul>
+                        <li>About</li>
+                        <li><a href="#">Our Story</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Brand Guidelines</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                    <ul>
+                        <li>Shop</li>
+                        <li><a href="#">Help</a></li>
+                        <li><a href="#">Downloads</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Return</a></li>
+                    </ul>
+                    <ul>
+                        <li>Terms and Conditions</li>
+                        <li><a href="#">All Terms and Policies</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div class="social">
+                <section class="media">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fa-brands fa-reddit-alien"></i></a>
+                    <a href="#"><i class="fa-brands fa-discord"></i></a>
+                    <i class="fa-brands fa-facebook-messenger"></i>
+                    <i class="fa-brands fa-whatsapp"></i>
+                </section>
+            </div>
+        </section>
+        <article>
+            <p>
+                Copyright @ 2023, All Right Reserved
+            </p>
+            <div class="lang">
+                <i class="fa-solid fa-caret-down"></i>
+                <p>English (EU)</p>
+                <ul class="list-lang">
+                    <li>English (EU)</li>
+                    <li>Spanish (MX)</li>
+                    <li>Korean (KR)</li>
+                </ul>
+            </div>
+        </article>
+    </div>
+    </footer>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'FooterSection'
+    }
+</script>

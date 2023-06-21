fd63a1f816329b183e6bdbaea88cd0b2bf3e5d00
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-06-20 19:04:42 -0700

    deleted footersection import

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <FooterSection/>
+  
 </template>
 
 <script>
-import FooterSection from './components/FooterSection.vue';
+
 
 export default {
   name: 'App',
   components: {
-    FooterSection
+    
   }
 }
 </script>

1f1ed7de65508b0f69b05b87cb3224e2f676023e
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-06-20 19:22:46 -0700

    added product component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  
+  <ProductSection/>
 </template>
 
 <script>
-
+import ProductSection from './components/ProductSection.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    ProductSection
   }
 }
 </script>
---
+++ src/components/ProductSection.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="product-body">
+        <div class="item-header">
+            <div>
+                <h1>Xbox One XS</h1>
+                <a href="index.html"><i class="fa-solid fa-arrow-left"></i></a>
+            </div>
+        </div>
+        <div class="product">
+            <div class="gallery">
+                <img src="https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2020/06/EverythingFuture_HERO.jpg?fit=1920%2C1080&ssl=1" alt="main-pic">
+                <div class="picList">
+                    <figure>
+                        <img src="https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2020/06/EverythingFuture_HERO.jpg?fit=1920%2C1080&ssl=1" alt="pic-1">
+                    </figure>
+                    <figure>
+                        <img src="https://cdn.vox-cdn.com/thumbor/GsrqvfzZ5o1iE0Tx_SBkw_d9WqA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22012144/jbareham_201020_ecl1040_xbox_lead_0001.jpg" alt="pic-2">
+                    </figure>
+                    <figure>
+                        <img src="https://images.hothardware.com/contentimages/newsitem/61287/content/Xbox_Series_X_Diablo_IV_Bundle_Package.jpg" alt="pic-3">
+                    </figure>
+                </div>
+            </div>
+            <div class="details-wrapper">
+                <div class="details">
+                    <h2>Xbox One XS</h2>
+                    <p>Game Console</p>
+                    <h3>CA$690.00</h3>
+                    <aside class="description">
+                        <p>The Xbox One is a gaming console by Microsoft with powerful graphics and a wide range of entertainment features.</p>
+                    </aside>
+                    <div class="options">
+                        <article>
+                            <aside>
+                                <i class="fa-solid fa-truck"></i>
+                                <h4>Free Delivery across Canada</h4>
+                            </aside>
+                            <h5>7 business days</h5>
+                        </article>
+                        <article>
+                            <aside>
+                                <i class="fa-solid fa-right-left"></i>
+                                <h4>Free Return</h4>
+                            </aside>
+                            <h5>Return for free within 30 days</h5>
+                        </article>
+                    </div>
+                    <div class="avail">
+                        Available
+                    </div>
+                    <div class="seller">
+                        <h3>Reviews</h3>
+                        <aside>
+                            <i class="fa-solid fa-circle"></i>
+                            <i class="fa-solid fa-circle"></i>
+                            <i class="fa-solid fa-circle"></i>
+                            <i class="fa-regular fa-circle"></i>
+                            <i class="fa-regular fa-circle"></i>
+                        </aside>
+                    </div>
+                    <div class="location">
+                        Vancouver, British Columbia
+                    </div>
+                    <div class="quantity">
+                        <aside>
+                            <span>Quantity:</span>
+                            <select name="qty" id="qty">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </aside>
+                        <small>( 99 Available )</small>
+                    </div>
+                    <div class="buy-wrapper">
+                        <div class="buy-item">Buy</div>
+                        <a href="#">
+                            <i class="fa-solid fa-bookmark"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-solid fa-share"></i>
+                        </a>
+                    </div>
+                </div>
+            </div> 
+        </div>
+        <div class="bottom-section">
+            <div class="comments">
+                <h2>Comment (1)</h2>
+                <figure>
+                    <img src="https://cdn.britannica.com/52/76652-050-F4A6B093/Edgar-Allan-Poe.jpg" alt="review-icon">
+                    <figcaption>
+                        <h3>Edgar Allan Poe</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui sapiente et commodi ut pariatur, possimus, minima praesentium vitae nihil dolorum temporibus ad minus rerum voluptate officia esse nulla delectus modi.!</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="seller-info">
+                <h2>Seller Info</h2>
+                <figure>
+                    <img src="https://media.npr.org/assets/img/2018/11/20/cred-alex-bailey-for-twentieth-c-fox_wide-0b9306aa3b0c130bb2782db974bd31e33db5687b-s1100-c50.jpg" alt="seller-pic">
+                    <figcaption>
+                        <h3>Freddie Mercury</h3>
+                    </figcaption>
+                </figure>
+                <article>
+                    <a href="#">
+                        <figure>
+                            <img src="https://e.snmc.io/lk/l/x/8860427e67b6a9fa827cf04329a84cb8/7832542" alt="seller-item-1">
+                            <figcaption>
+                                <h2>Lorem ipsum dolor sit.</h2>
+                                <h3>CA$120.60</h3>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <figure>
+                            <img src="https://e.snmc.io/lk/l/x/8860427e67b6a9fa827cf04329a84cb8/7832542" alt="seller-item-2">
+                            <figcaption>
+                                <h2>Lorem ipsum dolor sit.</h2>
+                                <h3>CA$120.60</h3>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <figure>
+                            <img src="https://e.snmc.io/lk/l/x/8860427e67b6a9fa827cf04329a84cb8/7832542" alt="seller-item-3">
+                            <figcaption>
+                                <h2>Lorem ipsum dolor sit.</h2>
+                                <h3>CA$120.60</h3>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <figure>
+                            <img src="https://e.snmc.io/lk/l/x/8860427e67b6a9fa827cf04329a84cb8/7832542" alt="seller-item-4">
+                            <figcaption>
+                                <h2>Lorem ipsum dolor sit.</h2>
+                                <h3>CA$120.60</h3>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </article>
+                <a href="#">See More <i class="fa-solid fa-angle-right"></i></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'ProductSection'
+    }
+</script>

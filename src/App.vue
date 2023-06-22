e49d40c2a313156b99b8e3f062a160673fab3cbc
Dayhun <lanna05@naver.com>
2023-06-21 22:20:37 -0700

    merge fixed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-  <HeaderComponent/>
+  
 </template>
 
 <script>
-import HeaderComponent from './components/HeaderComponent.vue';
+
+
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #2c3e50;
-  margin-top: 60px; */
-}
+@import url("../css/style.css");
 </style>
